2208ea6d1f7231c0f3785be1ed4491523a75c1c5
jasonp2009 <jason_pawar@hotmail.com>
2023-08-26 16:11:52 +1000

    Remove unused IFixture from test class

--- ApacBreachersRanked/Tests/ApacBreachersRanked.Domain.Tests/MMR/PlayerMMRTests.cs
+++ ApacBreachersRanked/Tests/ApacBreachersRanked.Domain.Tests/MMR/PlayerMMRTests.cs
@@ -2,15 +2,12 @@
 using ApacBreachersRanked.Domain.MMR.Enums;
 using ApacBreachersRanked.Domain.Tests.User;
 using ApacBreachersRanked.Domain.User.Interfaces;
-using AutoFixture;
 using FluentAssertions;
 
 namespace ApacBreachersRanked.Domain.Tests.MMR
 {
     public class PlayerMMRTests
     {
-        private readonly IFixture _fixture = new Fixture();
-
         [Theory]
         [InlineData(Rank.Silver, 1029, 10, Rank.Silver)]
         [InlineData(Rank.Silver, 1030, 10, Rank.Gold)]
