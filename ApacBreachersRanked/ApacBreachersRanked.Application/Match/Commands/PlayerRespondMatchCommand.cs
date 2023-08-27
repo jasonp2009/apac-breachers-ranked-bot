854f386e4be60248ec98615c0822903e405df90c
jasonp2009 <jason_pawar@hotmail.com>
2023-08-27 17:40:09 +1000

    Ban users for rejected or failing to confirm a match in time

--- ApacBreachersRanked/ApacBreachersRanked.Application/Match/Commands/PlayerRespondMatchCommand.cs
+++ ApacBreachersRanked/ApacBreachersRanked.Application/Match/Commands/PlayerRespondMatchCommand.cs
@@ -1,5 +1,6 @@
 ﻿using ApacBreachersRanked.Application.Common.Mediator;
 using ApacBreachersRanked.Application.DbContext;
+using ApacBreachersRanked.Application.Moderation.Commands;
 using ApacBreachersRanked.Application.Users;
 using ApacBreachersRanked.Domain.Match.Entities;
 using MediatR;
@@ -16,13 +17,16 @@ namespace ApacBreachersRanked.Application.Match.Commands
     public class PlayerConfirmMatchCommandHandler : ICommandHandler<PlayerRespondMatchCommand>
     {
         private readonly IDbContext _dbContext;
-        public PlayerConfirmMatchCommandHandler(IDbContext dbContext)
+        private readonly IMediator _mediator;
+        public PlayerConfirmMatchCommandHandler(IDbContext dbContext, IMediator mediator)
         {
             _dbContext = dbContext;
+            _mediator = mediator;
         }
         public async Task<Unit> Handle(PlayerRespondMatchCommand request, CancellationToken cancellationToken)
         {
             MatchPlayer? matchPlayer = await _dbContext.MatchPlayers
+                .Include(x => x.Match)
                 .FirstOrDefaultAsync(mp =>
                     mp.Match.Status == Domain.Match.Enums.MatchStatus.PendingConfirmation &&
                     mp.UserId == request.DiscordUserId.ToIUserId(),
@@ -36,6 +40,12 @@ namespace ApacBreachersRanked.Application.Match.Commands
             } else
             {
                 matchPlayer.Reject();
+                await _mediator.Send(new BanUserCommand
+                {
+                    DiscordUserId = matchPlayer.UserId.GetDiscordId(),
+                    Duration = TimeSpan.FromMinutes(15),
+                    Reason = $"Rejected match #{matchPlayer.Match.MatchNumber}"
+                });
             }
             await _dbContext.SaveChangesAsync(cancellationToken);
             return Unit.Value;
--- ApacBreachersRanked/ApacBreachersRanked.Application/Match/EventHandlers/AutoCancelMatchHandler.cs
+++ ApacBreachersRanked/ApacBreachersRanked.Application/Match/EventHandlers/AutoCancelMatchHandler.cs
@@ -1,4 +1,6 @@
 ﻿using ApacBreachersRanked.Application.DbContext;
+using ApacBreachersRanked.Application.Moderation.Commands;
+using ApacBreachersRanked.Application.Users;
 using ApacBreachersRanked.Domain.Match.Entities;
 using ApacBreachersRanked.Domain.Match.Events;
 using MediatR;
@@ -9,16 +11,30 @@ namespace ApacBreachersRanked.Application.Match.EventHandlers
     public class AutoCancelMatchHandler : INotificationHandler<AutoCancelMatchEvent>
     {
         private readonly IDbContext _dbContext;
+        private readonly IMediator _mediator;
         
-        public AutoCancelMatchHandler(IDbContext dbContext)
+        public AutoCancelMatchHandler(IDbContext dbContext, IMediator mediator)
         {
             _dbContext = dbContext;
+            _mediator = mediator;
         }
         public async Task Handle(AutoCancelMatchEvent notification, CancellationToken cancellationToken)
         {
-            MatchEntity match = await _dbContext.Matches.SingleAsync(match => match.Id == notification.MatchId);
+            MatchEntity match = await _dbContext.Matches
+                .Include(x => x.AllPlayers)
+                .SingleAsync(match => match.Id == notification.MatchId);
 
             match.AutoCancel();
+
+            foreach (MatchPlayer unconfirmedPlayer in match.AllPlayers.Where(x => !x.Confirmed))
+            {
+                await _mediator.Send(new BanUserCommand
+                {
+                    DiscordUserId = unconfirmedPlayer.UserId.GetDiscordId(),
+                    Duration = TimeSpan.FromMinutes(15),
+                    Reason = $"Failed to confirm match #{match.MatchNumber} within time limit"
+                });
+            }
 
             await _dbContext.SaveChangesAsync(cancellationToken);
         }
