06a0fa8abed246e1cf1a81ea969ec8cbb25d7a9b
jasonp2009 <jason_pawar@hotmail.com>
2023-08-31 18:12:38 +1000

    Remove ban on reject

--- ApacBreachersRanked/ApacBreachersRanked.Application/Match/Commands/PlayerRespondMatchCommand.cs
+++ ApacBreachersRanked/ApacBreachersRanked.Application/Match/Commands/PlayerRespondMatchCommand.cs
@@ -1,6 +1,5 @@
 ﻿using ApacBreachersRanked.Application.Common.Mediator;
 using ApacBreachersRanked.Application.DbContext;
-using ApacBreachersRanked.Application.Moderation.Commands;
 using ApacBreachersRanked.Application.Users;
 using ApacBreachersRanked.Domain.Match.Entities;
 using MediatR;
@@ -17,16 +16,13 @@ namespace ApacBreachersRanked.Application.Match.Commands
     public class PlayerConfirmMatchCommandHandler : ICommandHandler<PlayerRespondMatchCommand>
     {
         private readonly IDbContext _dbContext;
-        private readonly IMediator _mediator;
-        public PlayerConfirmMatchCommandHandler(IDbContext dbContext, IMediator mediator)
+        public PlayerConfirmMatchCommandHandler(IDbContext dbContext)
         {
             _dbContext = dbContext;
-            _mediator = mediator;
         }
         public async Task<Unit> Handle(PlayerRespondMatchCommand request, CancellationToken cancellationToken)
         {
             MatchPlayer? matchPlayer = await _dbContext.MatchPlayers
-                .Include(x => x.Match)
                 .FirstOrDefaultAsync(mp =>
                     mp.Match.Status == Domain.Match.Enums.MatchStatus.PendingConfirmation &&
                     mp.UserId == request.DiscordUserId.ToIUserId(),
@@ -40,12 +36,6 @@ namespace ApacBreachersRanked.Application.Match.Commands
             } else
             {
                 matchPlayer.Reject();
-                await _mediator.Send(new BanUserCommand
-                {
-                    DiscordUserId = matchPlayer.UserId.GetDiscordId(),
-                    Duration = TimeSpan.FromMinutes(15),
-                    Reason = $"Rejected match #{matchPlayer.Match.MatchNumber}"
-                });
             }
             await _dbContext.SaveChangesAsync(cancellationToken);
             return Unit.Value;
