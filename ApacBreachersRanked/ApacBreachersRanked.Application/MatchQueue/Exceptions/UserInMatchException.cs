58e1e765e3f7a05ad98b71d19f81f062f6b1e6e6
jasonp2009 <jason_pawar@hotmail.com>
2023-09-02 18:51:12 +1000

    Block users from joining the queue while still in a match

---
+++ ApacBreachersRanked/ApacBreachersRanked.Application/MatchQueue/Exceptions/UserInMatchException.cs
@@ -0,0 +1,21 @@
+﻿using ApacBreachersRanked.Domain.Match.Entities;
+using ApacBreachersRanked.Domain.User.Interfaces;
+
+namespace ApacBreachersRanked.Application.MatchQueue.Exceptions
+{
+    public class UserInMatchException : Exception, IUser
+    {
+        public IUserId UserId { get; private set; }
+        public string? Name { get; private set; }
+        public Guid MatchId { get; private set; }
+        public int MatchNumber { get; private set; }
+
+        public UserInMatchException(IUser user, MatchEntity match)
+        {
+            UserId = user.UserId;
+            Name = user.Name;
+            MatchId = match.Id;
+            MatchNumber = match.MatchNumber;
+        }
+    }
+}
---
+++ ApacBreachersRanked/ApacBreachersRanked.Application/MatchQueue/Queries/GetInProgressMatchByUserQuery.cs
@@ -0,0 +1,30 @@
+﻿using ApacBreachersRanked.Application.Common.Mediator;
+using ApacBreachersRanked.Application.DbContext;
+using ApacBreachersRanked.Domain.Match.Entities;
+using ApacBreachersRanked.Domain.Match.Enums;
+using ApacBreachersRanked.Domain.User.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApacBreachersRanked.Application.MatchQueue.Queries
+{
+    public class GetInProgressMatchByUserQuery : IQuery<MatchEntity?>
+    {
+        public IUserId UserId { get; set; } = null!;
+    }
+
+    public class IsUserInMatchHandler : IQueryHandler<GetInProgressMatchByUserQuery, MatchEntity?>
+    {
+        private readonly IDbContext _dbContext;
+
+        public IsUserInMatchHandler(IDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public Task<MatchEntity?> Handle(GetInProgressMatchByUserQuery request, CancellationToken cancellationToken)
+            => _dbContext.Matches.FirstOrDefaultAsync(
+                x => (x.Status == MatchStatus.PendingConfirmation ||
+                      x.Status == MatchStatus.Confirmed) &&
+                      x.AllPlayers.Any(x => x.UserId.Equals(request.UserId)),
+                cancellationToken);
+    }
+}
