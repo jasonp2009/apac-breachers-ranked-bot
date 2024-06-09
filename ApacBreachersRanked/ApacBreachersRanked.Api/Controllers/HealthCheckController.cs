5d6ffbd9b88af5b70871c21ecd22c4298cc15240
jasonp2009 <jason_pawar@hotmail.com>
2024-06-09 13:24:52 +1000

    Add healthcheck endpoint

---
+++ ApacBreachersRanked/ApacBreachersRanked.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApacBreachersRanked.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HealthCheckController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> HealthCheck()
+    {
+        return Ok("Healthy");
+    }
+}
