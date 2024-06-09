4629eec862a280d502e7163164e9470749592af6
jasonp2009 <jason_pawar@hotmail.com>
2024-06-09 15:14:01 +1000

    Serilog config

--- ApacBreachersRanked/ApacBreachersRanked.Api/Program.cs
+++ ApacBreachersRanked/ApacBreachersRanked.Api/Program.cs
@@ -1,3 +1,6 @@
+using Serilog;
+using Serilog.Formatting.Compact;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog((a, cfg) =>
+{
+    cfg.WriteTo.Console(new CompactJsonFormatter());
+});
 
 var app = builder.Build();
 
