8d89d2427141211974def62933096f1dd122a6d1
jasonp2009 <jason_pawar@hotmail.com>
2024-06-09 12:12:48 +1000

    API Project

---
+++ ApacBreachersRanked/ApacBreachersRanked.Api/Controllers/WebhookController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApacBreachersRanked.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WebhookController : ControllerBase
+{
+    private readonly ILogger<WebhookController> _logger;
+
+    public WebhookController(ILogger<WebhookController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost("DeleteUser")]
+    public async Task<IActionResult> DeleteUserAsync(object body)
+    {
+        _logger.LogInformation("Received Delete User request with {@RequestBody}", body);
+        return Ok();
+    }
+}
