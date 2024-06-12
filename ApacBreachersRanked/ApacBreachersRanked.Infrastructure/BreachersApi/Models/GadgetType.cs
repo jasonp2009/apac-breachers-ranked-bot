7093bc29a4d184f8c36dddced4856d6dddac1df4
jasonp2009 <jason_pawar@hotmail.com>
2024-06-12 20:27:17 +1000

    Weapon/Gadget Type enums

--- ApacBreachersRanked/ApacBreachersRanked.Infrastructure/BreachersApi/Models/GadgetType.cs
+++ ApacBreachersRanked/ApacBreachersRanked.Infrastructure/BreachersApi/Models/GadgetType.cs
@@ -2,5 +2,20 @@
 
 public enum GadgetType
 {
-    Flashbang
+    FragGrenade = 10,
+    SmokeGrenade = 11,
+    IncendiaryGrenade = 12,
+    HealSyringe = 13,
+    
+    BreachingFoam = 20,
+    Flashbang = 21,
+    CloakingDevice = 22,
+    Drone = 23,
+    
+    Tripwire = 30,
+    StaticFieldEmitter = 31,
+    ProximitySensor = 32,
+    DoorBlocker = 33,
+    
+    Emp = 40,
 }
