57b76c21749afc9c93b93fca7a72e4b63a00f986
jasonp2009 <jason_pawar@hotmail.com>
2024-06-16 18:55:47 +1000

    Add pasting of breachers tracker url

--- client-spa/src/api/generated/abrApiClient.ts
+++ client-spa/src/api/generated/abrApiClient.ts
@@ -71,6 +71,48 @@ export class AbrApiClient {
      * @param breachersUserId (optional) 
      * @return OK
      */
+    getUser(breachersUserId?: string | undefined): Promise<BreachersUser> {
+        let url_ = this.baseUrl + "/BreachersUser/GetUser?";
+        if (breachersUserId === null)
+            throw new Error("The parameter 'breachersUserId' cannot be null.");
+        else if (breachersUserId !== undefined)
+            url_ += "BreachersUserId=" + encodeURIComponent("" + breachersUserId) + "&";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_: RequestInit = {
+            method: "GET",
+            headers: {
+                "Accept": "text/plain"
+            }
+        };
+
+        return this.http.fetch(url_, options_).then((_response: Response) => {
+            return this.processGetUser(_response);
+        });
+    }
+
+    protected processGetUser(response: Response): Promise<BreachersUser> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            result200 = BreachersUser.fromJS(resultData200);
+            return result200;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<BreachersUser>(null as any);
+    }
+
+    /**
+     * @param breachersUserId (optional) 
+     * @return OK
+     */
     linkDiscordUser(breachersUserId?: string | undefined): Promise<void> {
         let url_ = this.baseUrl + "/BreachersUser/LinkDiscordUser?";
         if (breachersUserId === null)
