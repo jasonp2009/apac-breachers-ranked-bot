19b0e1345728c71054d4b443ace36d575de27e40
jasonp2009 <jason_pawar@hotmail.com>
2024-07-03 21:09:06 +1000

    Breachers API Controller

---
+++ ApacBreachersRanked/ApacBreachersRanked.Api/Controllers/BreachersApiController.cs
@@ -0,0 +1,27 @@
+﻿using ApacBreachersRanked.Infrastructure.Breachers.Api;
+using ApacBreachersRanked.Infrastructure.Breachers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApacBreachersRanked.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BreachersApiController : ControllerBase
+{
+    private readonly BreachersApiClient _breachersApiClient;
+
+    public BreachersApiController(BreachersApiClient breachersApiClient)
+    {
+        _breachersApiClient = breachersApiClient;
+    }
+    
+    [HttpGet("get_match_data")]
+    [ProducesResponseType<IEnumerable<GetMatchResponse>>(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetMatchData(
+        [FromQuery] string player_id,
+        CancellationToken cancellationToken)
+    {
+        IEnumerable<GetMatchResponse> response = await _breachersApiClient.GetMatchesByUserId(player_id, cancellationToken);
+        return Ok(response);
+    }
+}
