c492a70327da770c1d87eb83977711b836ba82a5
jasonp2009 <jason_pawar@hotmail.com>
2024-10-05 17:25:13 +1000

    Improve performance mmr (#44)
    * Fix performance MMR
    
    * 0 Score check

--- ApacBreachersRanked/ApacBreachersRanked.Application/MMR/Services/PerformanceMmrAdjustmentService.cs
+++ ApacBreachersRanked/ApacBreachersRanked.Application/MMR/Services/PerformanceMmrAdjustmentService.cs
@@ -28,7 +28,7 @@ public class PerformanceMmrAdjustmentService : IMmrAdjustmentService
         var mmrAdjustments = (await _mmrAdjustmentService.CalculateAdjustmentsAsync(match, playerMmrsList, cancellationToken)).ToList();
         var gameData =
             await _dbContext.GameData.FirstOrDefaultAsync(gameData => gameData.MatchId == match.Id, cancellationToken);
-        if (gameData is null)
+        if (gameData is null || gameData.HomePlayers.All(homePlayer => homePlayer.Score == 0) || gameData.AwayPlayers.All(awayPlayer => awayPlayer.Score == 0))
         {
             return mmrAdjustments;
         }
@@ -99,6 +99,6 @@ public class PerformanceMmrAdjustmentService : IMmrAdjustmentService
     {
         var gamePlayerData = gameData.Players.First(gamePlayer => gamePlayer.UserId.Equals(player));
         var teamPlayerData = gameData.Players.Where(gamePlayer => gamePlayer.Side == gamePlayerData.Side);
-        return (decimal)gamePlayerData.Score / teamPlayerData.Sum(x => x.Score);
+        return (decimal)gamePlayerData.Score / ((decimal)teamPlayerData.Average(x => x.Score) * 2);
     }
 }
